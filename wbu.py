efd0128efbca0809418b3ebe40d0248e1c4582a9
Deshine <duc.hacker.ngo@gmail.com>
2024-03-20 22:49:18 +0700

    Update wbu.py

--- wbu.py
+++ wbu.py
@@ -7,17 +7,6 @@ urllib3.disable_warnings()
 truststore.inject_into_ssl()
 
 def main():
-    print(r"""
- __        __          _                _      _   _      _     
- \ \      / /_ _ _   _| |__   __ _  ___| | __ | | | |_ __| |___ 
-  \ \ /\ / / _` | | | | '_ \ / _` |/ __| |/ / | | | | '__| / __|
-   \ V  V / (_| | |_| | |_) | (_| | (__|   <  | |_| | |  | \__ \
-    \_/\_/ \__,_|\__, |_.__/ \__,_|\___|_|\_\  \___/|_|  |_|___/
-                 |___/                                          
-
-                 Author: deshine (https://hackerone.com/deshine)
-""")
-    
     parser = argparse.ArgumentParser(description="Usage:")
     parser.add_argument('-u', '--url', help='target url (e.g https://example.com)', required=True)
 
