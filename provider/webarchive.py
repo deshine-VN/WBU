1af0a5ac1ffb9dcaf67e50ccf410a538107a4153
Deshine <156778163+deshine-VN@users.noreply.github.com>
2024-03-21 11:11:12 +0700

    Update webarchive.py

--- provider/webarchive.py
+++ provider/webarchive.py
@@ -1,10 +1,16 @@
+from time import sleep
 import requests
 import config
 
 class WebArchive:
     @staticmethod
     def query(url):
-        result_urls = requests.get("https://web.archive.org/cdx/search/cdx?url={}/*&output=json&collapse=urlkey&fl=original".format(url), headers=config.headers).json()
+        while True:
+            response = requests.get("https://web.archive.org/cdx/search/cdx?url={}/*&output=json&collapse=urlkey&fl=original".format(url), headers=config.headers)
+            if response.status_code == 200:
+                break
+            sleep(1)
+        result_urls = response.json()
         if result_urls:
             result_urls = result_urls[1:]
         result_urls = [item[0] for item in result_urls]
